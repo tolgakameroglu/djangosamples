24cd6e7c2d8d23aa0207db78c2743a914f18a594
tolgakameroglu <tolga_kameroglu@hotmail.com>
2022-10-11 22:33:10 +0300

    dskngjkrsng

---
+++ lecture3/newyear/views.py
@@ -0,0 +1,12 @@
+import datetime
+
+from django.shortcuts import render
+
+
+def index(request):
+    now = datetime.datetime.now()
+    return render(request,"newyear/index.html",{
+        "newyear" : now.month == 1 and now.year == 1,
+        "now" : now
+    })
+
